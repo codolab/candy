047c531760d00f0553be029e45af9bbc8cb739ec
tranbathanhtung <tranbathanhtung222@gmail.com>
2020-11-05 19:04:24 +0700

    clsx support

--- packages/moon-engine/src/createClassParse/index.js
+++ packages/moon-engine/src/createClassParse/index.js
@@ -1,22 +1,41 @@
+import clsx from "./clsx";
 import { createLookup } from "./lookup";
 import { Configuration } from "../configure";
 
 const noop = () => null;
 
-export const createClassParse = (processSystem = noop, _lookupWithVariant = noop) => {
+const checkIsTag = (val, ...rest) =>
+  !!(
+    Array.isArray(val) &&
+    val.raw &&
+    val.raw.length === val.length &&
+    Object.isFrozen(val) &&
+    rest.length + 1 === val.length
+  );
+
+export const createClassParse = (
+  processSystem = noop,
+  _lookupWithVariant = noop
+) => {
   const lookup = createLookup(processSystem);
   const lookupWithVariant = _lookupWithVariant;
 
-  return function parse(strings, ...exprs) {
+  return function parse(values, ...exprs) {
+    let strings = values;
+    const isTag = checkIsTag(values, ...exprs);
+    if (!isTag) {
+      strings = [clsx(values, ...exprs)];
+    }
     const rules = strings
       .reduce(
-        (str, rule, i) => (str += [rule || "", exprs[i] || ""].join(" ")),
+        (str, rule, i) =>
+          (str += [rule || "", (isTag && exprs[i]) || "" || ""].join(" ")),
         ""
       )
       .replace(/\s\s+/g, " ")
       .trim()
       .split(" ");
-
+      
     const styles = rules.reduce((acc, val) => {
       if (!val) return acc;
       const variant = val.match(/.*:/g) ? val.match(/.*:/g)[0] : "";
