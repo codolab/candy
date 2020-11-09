f90a5077c4df6196dd3b5ca919ba290deda95025
tranbathanhtung <tranbathanhtung222@gmail.com>
2020-11-09 16:19:56 +0700

    system

--- packages/system/rollup.config.js
+++ packages/system/rollup.config.js
@@ -1,45 +1,6 @@
-import babel from "@rollup/plugin-babel";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import filesize from "rollup-plugin-filesize";
-
 import pkg from "./package.json";
+import { createCoreConfig } from "../../rollup.common";
 
-const { PRODUCTION } = process.env;
+const core = createCoreConfig({ pkg });
 
-export default [
-  {
-    input: "src/index.js",
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      babel({
-        exclude: /node_modules/,
-        presets: ["@babel/preset-env"],
-        sourceMaps: true,
-      }),
-      terser(),
-      filesize(),
-    ],
-    output: [
-      { file: pkg.main, format: "cjs", sourcemap: true, exports: "named" },
-    ],
-  },
-  {
-    input: "src/index.js",
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      babel({
-        exclude: /node_modules/,
-        babelHelpers: "runtime",
-        sourceMaps: true,
-        presets: ["@babel/preset-env"],
-        plugins: [["@babel/transform-runtime", { useESModules: true }]],
-      }),
-      terser(),
-    ],
-    output: [{ file: pkg.module, format: "esm", sourcemap: true }],
-  },
-];
+export default [core];
