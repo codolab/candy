681f902013282f49e70fa557c24842bfae112cd5
tranbathanhtung <tranbathanhtung222@gmail.com>
2020-11-10 20:46:42 +0700

    test:perf

--- benchmarks/lib/goober.js
+++ benchmarks/lib/goober.js
@@ -6,21 +6,21 @@ const app = document.createElement("div");
 
 module.exports = () => {
   const counter = Math.random();
-  const className = css`
-    opacity: ${counter > 0.5 ? 1 : 0};
-    font-family: inherit;
-    font-size: inherit;
-    display: inline-block;
-    margin: 0;
-    padding: 16px;
-    border: 0;
-    border-radius: 4px;
-    color: white;
-    appearance: none;
-    &:hover {
-      background-color: black;
-    }
-  `;
+  const className = css({
+    opacity: counter > 0.5 ? 1 : 0,
+    fontFamily: "inherit",
+    fontSize: "inherit",
+    display: "inline-block",
+    margin: 0,
+    padding: 16,
+    border: 0,
+    borderRadius: 4,
+    color: "white",
+    appearance: "none",
+    "&:hover": {
+      backgroundColor: "black",
+    },
+  });
 
   const button = render(
     h(({ children }) => children, { className }, "Hello World"),
