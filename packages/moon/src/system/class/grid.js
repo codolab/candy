c2f6c0409ddc853e354edd8c9b0c21ef289e8b07
Trần Bá Thanh Tùng <tranbathanhtung222@gmail.com>
2020-11-27 04:19:08 +0000

    warning  grid classes

--- packages/moon/src/system/class/grid.js
+++ packages/moon/src/system/class/grid.js
@@ -1,46 +1,60 @@
-import { system } from "candy-system";
+import { system, get } from "candy-system";
+
+const transform = (val, scale) => {
+  return get(scale, val, null);
+}
 
 export const config = {
   // class syntax
   gridCols: {
     property: "grid-template-columns",
     scale: "gridTemplateColumns",
+    transform,
   },
   col: {
     property: "grid-column",
     scale: "gridColumn",
+    transform,
   },
   colSpan: {
     property: "grid-column",
     scale: "gridColumn",
+    transform,
   },
   colStart: {
     property: "grid-column-start",
     scale: "gridColumn",
+    transform,
   },
   colEnd: {
     property: "grid-column-end",
     scale: "gridColumn",
+    transform,
   },
   gridRows: {
     property: "grid-template-rows",
     scale: "gridTemplateRows",
+    transform,
   },
   row: {
     property: "grid-row",
     scale: "gridRow",
+    transform,
   },
   rowSpan: {
     property: "grid-row",
     scale: "gridRow",
+    transform,
   },
   rowStart: {
     property: "grid-row-start",
     scale: "gridRow",
+    transform,
   },
   rowEnd: {
     property: "grid-row-end",
     scale: "gridRow",
+    transform,
   },
   gridFlow: {
     property: "grid-auto-flow",
@@ -55,25 +69,34 @@ export const config = {
         case "row":
           return "row";
         default:
-          return val;
+          return null;
       }
     },
   },
   autoCols: {
     property: "grid-auto-columns",
     scale: "gridAutoColumns",
+    transform,
   },
   autoRows: {
     property: "grid-auto-rows",
     scale: "gridAutoRows",
+    transform,
   },
   gapX: {
     property: "column-gap",
     scale: "gap",
+    transform,
   },
   gapY: {
     property: "row-gap",
     scale: "gap",
+    transform,
+  },
+  gap: {
+    property: "gap",
+    scale: "gap",
+    transform,
   },
 };
 
