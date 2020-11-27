d3b7529be56de2cce5749f64634131e058043245
Trần Bá Thanh Tùng <tranbathanhtung222@gmail.com>
2020-11-27 08:53:09 +0000

    warning duplicate classes

--- packages/moon-engine/src/createClassParser/index.js
+++ packages/moon-engine/src/createClassParser/index.js
@@ -29,6 +29,7 @@ export const createClassParser = (
       if (!isTag) {
         strings = [clsx(values, ...exprs)];
       }
+
       const rules = strings
         .reduce(
           (str, rule, i) =>
@@ -38,14 +39,20 @@ export const createClassParser = (
         .replace(/\s\s+/g, " ")
         .trim()
         .split(" ");
-
+      
+      const alreadySeen = {};
       const styles = rules.reduce((acc, val) => {
         if (!val) return acc;
         const variant = val.match(/.*:/g) ? val.match(/.*:/g)[0] : "";
         const className = val.replace(variant, "");
+        if (alreadySeen[val]) 
+          warn(`Duplicate class "${val}"`);
+
+        alreadySeen[val] = true;
         const translated = lookup(className);
         if (!translated || Object.keys(translated).length === 0) 
-          warn(`Can't translate "${className}"`)
+          warn(`Can't translate "${className}"`);
+
         const translatedWithVariant = lookupWithVariant(
           variant,
           translated,
